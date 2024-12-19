4e708ea9d1e52e38dbd8653f667c96a407be9d07
Viggo Mikaelsson <h24vigmi@du.se>
2024-12-19 14:38:11 +0100

    Små ändringar + extra
    gjort små ändringar på programmet och lagt till lite grejer så att vi ska vara klara med sprint 1. Det saknas några grejer men jag tror på dig Linus.

--- lab4/Hair.cs
+++ lab4/Hair.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace lab4
 {
-    public struct Hair ()
+    public struct Hair
     {
-        string hairLength = "10cm";
-        string hairColor = "blå";
-        string hairType = "curly";
+        // Make fields public or provide properties to access them
+        public string HairLength;
+        public string HairColor;
+        public string HairType;
 
+        // Constructor to initialize the fields
+        public Hair(string hairLength, string hairColor, string hairType)
+        {
+            HairLength = hairLength;
+            HairColor = hairColor;
+            HairType = hairType;
+        }
+        public override string ToString()
+        {
+            return $"Hair Length: {HairLength}, Hair Color: {HairColor}, Hair Type: {HairType}";
+        }
     }
 }
--- lab4/Program.cs
+++ lab4/Program.cs
@@ -4,8 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Genders gen = Genders.Nonbinary; 
-            Hair hair = new Hair();
+            Genders myGender = Genders.Nonbinary;
+            Console.WriteLine(myGender.ToString());
+            Hair myHair = new Hair("10cm", "blå", "curly");
+            Console.WriteLine(myHair.ToString());
+            Person myBirthday = new Person();
+            myBirthday.Birthday = "1925-06-13";
+            Console.WriteLine(myBirthday.Birthday);
+            Person myEyeColor = new Person();
+            myEyeColor.EyeColor = "blå";
+            Console.WriteLine(myEyeColor.EyeColor);
+        }
+    }
+    public class Person
+    {
+        public Genders CurrentGender { get; set; }
+        public Hair CurrentHair { get; set; }
+        private string birthday;
+        public string Birthday
+        {
+            get { return birthday; }
+            set { birthday = value; }
+        }
+        private string eyeColor;
+        public string EyeColor
+        {
+            get { return eyeColor; }
+            set { eyeColor = value; }
         }
     }
 }
