dd2fbb56c3147d54a382011a4f29a50c9f3f6088
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2025-01-07 14:37:31 +0100

    Added debug

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            bool runDebug = true;
+            Debug debug = new Debug();
+            if (runDebug)
+            {
+                debug.RunDebug();
+            }
             Menu menu = new Menu();
             menu.GoToMenu();
 
