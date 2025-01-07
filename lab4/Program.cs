a9b5d9530f97302bfc75afb04691b6a47a72c06c
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2025-01-07 10:23:21 +0100

    .

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            List<Person> listOfPeople = new List<Person>();
             Hair hair = new Hair("long", "blue", "stright");
             Person person = new Person(Genders.Nonbinary,hair,DateTime.Today,"red");
             Console.WriteLine(person);
