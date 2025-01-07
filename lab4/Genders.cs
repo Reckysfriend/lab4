9bf1ddde8837f8d53bc9cb6b742e8ac4c94b51af
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2025-01-07 08:11:57 +0100

    .

--- lab4/Genders.cs
+++ lab4/Genders.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace lab4
+﻿namespace lab4
 {
     public enum Genders
     {
